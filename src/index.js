3ea2aad615423c3ce237af5224f68ccce9c9b971
Samy Pessé <samypesse@gmail.com>
2015-04-09 10:11:48 +0200

    Adapt to browserify

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
-define([
-    "bower_components/coffee-script/extras/coffee-script"
-], function(coffeeScript) {
-    var commands = codebox.require("core/commands");
-    var File = codebox.require("models/file");
+var coffeeScript = require("coffee-script");
 
-    commands.register({
-        id: "coffeescript.preview",
-        title: "CoffeeScript: Preview",
-        context: ["editor"],
-        shortcuts: [
-            "ctrl+shift+c"
-        ],
-        run: function(args, context) {
-            var name = context.model.get("name").replace(context.model.getExtension(), ".js");
-            var code, error;
+var commands = codebox.require("core/commands");
+var File = codebox.require("models/file");
 
-            try {
-                code = coffeeScript.compile(context.getContent());
-            } catch (e) {
-                error = e;
-                code = e.toString();
-                name = "Error at compilation";
-            }
+commands.register({
+    id: "coffeescript.preview",
+    title: "CoffeeScript: Preview",
+    context: ["editor"],
+    shortcuts: [
+        "ctrl+shift+c"
+    ],
+    run: function(args, context) {
+        var name = context.model.get("name").replace(context.model.getExtension(), ".js");
+        var code, error;
 
-            var f = File.buffer(name, code);
-            return commands.run("file.open", {
-                file: f
-            })
-            .then(function() {
-                if (error) throw error;
-            });
+        try {
+            code = coffeeScript.compile(context.getContent());
+        } catch (e) {
+            error = e;
+            code = e.toString();
+            name = "Error at compilation";
         }
-    });
+
+        var f = File.buffer(name, code);
+        return commands.run("file.open", {
+            file: f
+        })
+        .then(function() {
+            if (error) throw error;
+        });
+    }
 });
